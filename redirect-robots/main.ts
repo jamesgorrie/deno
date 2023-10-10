c0bb6ddf53abe01813932378373937946f58a6ce
James Gorrie <jamesgorrie@users.noreply.github.com>
2023-10-10 09:05:28 +0100

    redirect-robots

--- redirect-robots/main.ts
+++ redirect-robots/main.ts
@@ -1,4 +1,4 @@
-const redirect = (xRobotsTag: "index" | "noindex", path: string) => new Response("", {
+const redirect = (xRobotsTag: "index" | "noindex" | "", path: string) => new Response("", {
   status: 307,
   headers: {
     Location: `https://loud-dodo-69.deno.dev${path}`,
@@ -25,9 +25,20 @@ Deno.serve(async (request: Request) => {
   if (request.url.match("/redirect-robots/redirect-with-noindex/2")) {
     return redirect("noindex", "/redirect-robots/redirect-with-noindex/3");
   }
-
   if (request.url.match("/redirect-robots/redirect-with-noindex/3")) {
     return new Response("Not indexable", {
+      status: 200,
+    });
+  }
+
+  if (request.url.match("/redirect-robots/redirect-with-unset-index/1")) {
+    return redirect("", "/redirect-robots/redirect-with-unset-index/2");
+  }
+  if (request.url.match("/redirect-robots/redirect-with-unset-index/2")) {
+    return redirect("", "/redirect-robots/redirect-with-unset-index/3");
+  }
+  if (request.url.match("/redirect-robots/redirect-with-unset-index/3")) {
+    return new Response("Not set", {
       status: 200,
     });
   }
